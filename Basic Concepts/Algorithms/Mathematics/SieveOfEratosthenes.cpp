045bc97f206cbae05aa98abab92ebe92f50bf141
Deepak Sood <deepaksood619@gmail.com>
2017-10-09 00:46:15 +0530

    FastIO templates

--- Basic Concepts/Algorithms/Mathematics/SieveOfEratosthenes.cpp
+++ Basic Concepts/Algorithms/Mathematics/SieveOfEratosthenes.cpp
@@ -11,12 +11,12 @@ void printSieve(int n) {
     bool prime[n+1];
     memset(prime, true, sizeof(prime));
         
-    for(int i = 2; i < n; i++)
+    for(int i = 2; i <= n; i++)
         if(prime[i])
-            for(int j = i+i; j < n; j+=i)
+            for(int j = i+i; j <= n; j+=i)
                 prime[j] = false;
     
-    for(int i = 2; i < n; i++)
+    for(int i = 2; i <= n; i++)
         if(prime[i])
             std::cout << i << " ";
     
---
+++ FastIO.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+// #include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    cout << "\n";
+
+    return 0;
+}
